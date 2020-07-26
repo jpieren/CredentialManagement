f73d13bcb33890297837f024633dbbb49467e50c
Jay Pieren <jay@pieren.name>
2020-07-26 13:41:24 +0200

    added machine to config

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 The module is designed to store and retrieve a password safely. The stored password is either encrypted with users session private key or on local machine level.
 User session based encryption is preferred. However using windows remote management winrm from Linux there are issue with user session based approach and machine level encryption/decryption may solve the issue.
 
-The password hashes are stored in the location $PwdHashPath. Directory is created if needed. The variable $Environment is used to have multiple password for the same id for different environments, like dev, qa and prod. If not used just leave it prod.
+The password hashes are stored in the location $PwdHashPath. Directory is created if needed. 
+The variable $Environment is used to have multiple password for the same id for different environments, like dev, qa and prod. If not used just leave it prod.
+The variable $System is used to define the crendtials set for a defined. THis is important if you run CI/CD envroment and all code is centrally.
 
 By executing the code below the module prompts for a password if a non valid hash or no hash exists.
 
--- sample-use.ps1
+++ sample-use.ps1
@@ -8,14 +8,14 @@ if (Test-Path $PwdHashPath) {New-Item -Type Directory -Force -Path $PwdHashPath 
 
 #Username/Password with hash from current session user
 $AdminUserSample        = "adminTest"
-$AdminUserSamplehash    = Get-PasswordHash -id $AdminUserSample -Environment $Environment -EnvironmentUser $env:USERNAME -HashPath $PwdHashPath
+$AdminUserSamplehash    = Get-PasswordHash -id $AdminUserSample -Environment $Environment -EnvironmentUser $env:USERNAME -System $env:COMPUTERNAME -HashPath $PwdHashPath
 
 #create credential object
 $credentialAdminSample = New-Object System.Management.Automation.PSCredential $AdminUserSample, $AdminUserSamplehash
 
 #Username/Password with hash from current machine
 $AdminUserSample2        = "adminTest2"
-$AdminUserSamplehash2    = Get-PasswordHash -id $AdminUserSample2 -Environment $Environment -EnvironmentUser $env:USERNAME -HashPath $PwdHashPath -Machine $true
+$AdminUserSamplehash2    = Get-PasswordHash -id $AdminUserSample2 -Environment $Environment -EnvironmentUser $env:USERNAME  -System $env:COMPUTERNAME -HashPath $PwdHashPath -Machine $true
 
 #create credential object
 $credentialAdminSample2 = New-Object System.Management.Automation.PSCredential $AdminUserSample2, $AdminUserSamplehash2
